4d9a056f24a3f8c749521a5adf6944feada1059d
Andrew <andrew.low@berkeley.edu>
2015-10-11 09:59:57 -0700

    final

---
+++ background.js
@@ -0,0 +1,9 @@
+
+function changeBackground(){
+
+document.body.style.backgroundImage = "url('https://media.giphy.com/media/UmO1IuCwW14k/giphy.gif')";
+document.body.style.backgroundSize = "120px 120px";
+}
+
+
+changeBackground();
