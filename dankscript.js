58fcd136e17caac18e884c528028c60e255f2bde
Andrew <andrew.low@berkeley.edu>
2015-10-11 02:58:53 -0700

    Form successfully calls replaceword function, but page does not refresh

--- dankscript.js
+++ dankscript.js
@@ -1,6 +1,7 @@
-console.log('called');
+console.log('calleddankscript');
 
 function replaceTextOnPage(from, to){
+  console.log('calledReplaceText');
   getAllTextNodes().forEach(function(node){
     node.nodeValue = node.nodeValue.replace(new RegExp(quote(from), 'g'), to);
   });
@@ -25,25 +26,35 @@ function replaceTextOnPage(from, to){
 }
 
 function dankReplace() { 
+  console.log(document.getElementById('input'));
   var search = document.getElementById('input').value;
   var repl = document.getElementById('rplc').value;
   replaceTextOnPage(search, repl);
   console.log("Search: " + search + ", Repl: " + repl); 
   return false; 
 }
+/*
+document.addEventListener('load', function(){
+  console.log('load happened');
+  document.getElementById('subby').addEventListener('click', dankReplace);
+});
+*/
 
-//replaceTextOnPage('Chiang', 'BOYU');
+replaceTextOnPage('Chiang', 'BOYU');
+console.log("Reached 1");
+//dankReplace();
+document.getElementById('subby').addEventListener('click', dankReplace);
+
+
 //This is tied to the run_at in manifest and to background
 //window.addEventListener("load", function(){replaceTextOnPage('Kevin', 'ANDREW');}, false)
 
+/*
 window.onload = function() {
   document.getElementById('subby').onclick = function() {
     console.log('click');
   }
 }
 
-document.addEventListener('load', function(){
-  console.log('load happened');
-  //document.getElementById('subby').addEventListener('click', dankReplace);
-});
+*/
 
